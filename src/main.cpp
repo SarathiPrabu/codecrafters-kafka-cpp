23eecd2adb5d7f8437c0da2e22ace34c372451e3
Sarathi Prabu <sarathiprabum@gmail.com>
2025-02-22 02:46:24 -0500

    Serial requests

--- src/main.cpp
+++ src/main.cpp
@@ -15,29 +15,26 @@ int main()
         server.start();
 
         struct sockaddr_in client_addr{};
-        
-        // while (1)
-        // {
-            int client_fd = server.acceptClient(client_addr);
-            std::vector<uint8_t> buffer(MAX_BUFFER_SIZE);
+        int client_fd = server.acceptClient(client_addr);
+        std::vector<uint8_t> buffer(MAX_BUFFER_SIZE);
 
-            if (Connection::receiveMessage(client_fd, buffer) > 0)
-            {
-                RequestMessage request = RequestMessage::parse(buffer);
+        while(Connection::receiveMessage(client_fd, buffer) > 0)
+        {
+            RequestMessage request = RequestMessage::parse(buffer);
 
-                ResponseMessage response{};
-                response.correlation_id = request.correlation_id;
-                response.error_code = (request.api_version < 0 | request.api_version > 4 )? 35 : 0; 
-                response.api_keys = 2;
-                response.api_key = 18;
-                response.min_version = 0;
-                response.max_version = 4;
-                response.throttle_time_ms = 0;
+            ResponseMessage response{};
+            response.correlation_id = request.correlation_id;
+            response.error_code = (request.api_version < 0 | request.api_version > 4 )? 35 : 0; 
+            response.api_keys = 2;
+            response.api_key = 18;
+            response.min_version = 0;
+            response.max_version = 4;
+            response.throttle_time_ms = 0;
 
-                Connection::sendResponse(client_fd, response);
-                close(client_fd);
-            }
-        // }
+            Connection::sendResponse(client_fd, response);
+        }
+        close(client_fd);
+    
     }
     catch (const std::exception &e)
     {
